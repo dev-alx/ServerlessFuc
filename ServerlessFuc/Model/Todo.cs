78692503302e9cca3520adf9ab601519fd8f8d1c
Alexander Acevedo <alx.acero@gmail.com>
2023-02-22 05:48:41 -0600

    Rest API with Azure functions

---
+++ ServerlessFuc/Model/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessFuc.Model
+{
+    public class Todo
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class TodoCreateModel
+    {
+        public string TaskDescription { get; set; }
+    }
+    public class TodoUpdateModel
+    {
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
